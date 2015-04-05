4e24ef532c0b7523f238fbee515aaf67370e5554
vinh3928 <vnguyen.cheme@gmail.com>
2015-04-04 19:49:56 -0600

    fixed menu animation

--- website-3/js/app.js
+++ website-3/js/app.js
@@ -1,8 +1,26 @@
-$(document).ready(function(){
-    $(".menu-bar, .icon-menu h1").click(function(){
-        $(".menu").animate({left: "0px"}, 200);
-        $("body").animate({left: "620px"}, 200);
-    });
-    
+var main = function() {
+  /* Push the body and the nav over by 285px over */
+  $('.menu-bar, .icon-menu h1').click(function() {
+    $('.menu').animate({
+      left: "0px"
+    }, 200);
 
-});
+    $('body').animate({
+      left: "320px"
+    }, 200);
+  });
+
+  /* Then push them back */
+  $('.icon-close').click(function() {
+    $('.menu').animate({
+      left: "-320px"
+    }, 200);
+
+    $('body').animate({
+      left: "0px"
+    }, 200);
+  });
+};
+
+
+$(document).ready(main);
